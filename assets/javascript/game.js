a98625c1855adbe2fbf01c7bef1dc4dc7abb5988
Lindsey Fischer <lindsfisch@gmail.com>
2017-04-23 21:17:09 -0500

    updated with displayltrat function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var words= ["calf", "hatchling", "pup", "cub","chick", "kid", "kit", "fawn", "fl
 var blank = "";
 var wrongGuess = [];
 var guessRemain = 7;
-var positions= [];
+
 
 
 //pick random word from array 
@@ -16,39 +16,67 @@ for (var i = 0; i < answer.length; i++) {
  	}
 
 document.onkeyup = function(event) {
- 	var userGuess = String.fromCharCode(event.key).toLowerCase();
+
+ 	var userGuess = event.key;
 
  	//populate the blank spaces on screen
 	document.getElementById("blanks").textContent = blank;
-	
- }
 
-//determines if userGuess is within answer
-function checkGuess(userGuess) {
+	console.log(userGuess);
+	checkGuess(userGuess);
+
+	//determines if userGuess is within answer
+	function checkGuess(userGuess) {
+    
     //guess within answer
-    if (answer.indexOf(userGuess) > -1) {
-        for (var i = 0; i < answer.length; i++) {
-            //checking where the index of answer and userGuess are the same
-            if (answer[i] === userGuess) {
-                positions.push(i);
-            }
-
+    	if (answer.indexOf(userGuess) > -1) {
+       		for (var i = 0; i < answer.length; i++) {
+            
+            //checking where the index of answer and userGuess are the same 
+            	if (answer[i] === userGuess) {
+            		displayLtrAt(userGuess, i);
+            		endGame ();
+           	 }
         }
     //guess not within answer
-    } else {
-		wrongGuess.push(userGuess);
-		document.getElementById("wrongGuesses").textContent = wrongGuess;
-		guessRemain--;
-		document.getElementById("remain").textContent = guessRemain;    	
+    	} else {
+    		//push to wrongGuess array
+			wrongGuess.push(userGuess);
+			document.getElementById("wrongGuesses").textContent = wrongGuess;
+			//subtract 1 from guesses remaining
+			guessRemain--;
+			document.getElementById("remain").textContent = guessRemain;
+			endGame ();    	
     }
+};
+ }
+
+function displayLtrAt(letter, index) {
+	//new variable
+	var newTxt = "";
+	//iterating over blank
+	for (var i = 0; i < blank.length; i++) 
+		
+		//if position in blank and where index of answer and userGuess are the same
+		//append the userGuess letter
+		if (i === index) {
+			newTxt += letter;
+
+		//else keep whatever is in current position	
+		} else {
+			newTxt += blank[i];
+		}
+	blank = newTxt;
+
+		document.getElementById("blanks").textContent = blank;
+	};
+	
+function endGame () {
+	if (guessRemain === 0 || blank.indexOf("_") === -1) {
+		console.log("over");
+	}
 };
 
-function displayLtr(ltr) {
-    for (var i = 0; i < positions.length; i++) {
-        blank.charAt(i) = ltr;
-        document.getElementById("blanks").textContent = blank;
-    }
-};
 
 
 
